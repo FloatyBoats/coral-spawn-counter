37e4c798246b1f7392d0b479f2bbd0bc128a7ec8
Alistair English <alistair.english@protonmail.com>
2022-08-31 15:43:38 +1000

    add opencv

--- android_app/app/src/main/java/com/example/coralspawncounter/MainActivity.kt
+++ android_app/app/src/main/java/com/example/coralspawncounter/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.coralspawncounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import org.opencv.android.OpenCVLoader
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if(OpenCVLoader.initDebug()){
+            Log.d("OpenCv Integrated:","true")
+        } else{
+            Log.d("OpenCv Integrated:","false")
+        }
         setContentView(R.layout.activity_main)
     }
 }
