4c95b0e9e8eb5c508860c8c51812b45b2daa6a11
Alistair English <alistair.english@protonmail.com>
2023-03-01 11:14:20 +1000

    hook up camera

--- CoralSpawnCounter/app/src/main/java/com/example/coralspawncounter/ui/RecordScreen.kt
+++ CoralSpawnCounter/app/src/main/java/com/example/coralspawncounter/ui/RecordScreen.kt
@@ -1,12 +1,26 @@
 package com.example.coralspawncounter.ui
 
+import android.content.Context
+import android.util.Log
+import android.view.ViewGroup
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
 import com.google.accompanist.permissions.shouldShowRationale
+import kotlinx.coroutines.*
+import java.util.concurrent.Executor
+import kotlin.coroutines.*
 
 
 @OptIn(ExperimentalPermissionsApi::class)
@@ -18,7 +32,7 @@ fun RecordScreen() {
     )
 
     if (cameraPermissionState.status.isGranted) {
-        Text("Camera permission Granted")
+        CameraPreviewView()
     } else {
         Column {
             val textToShow = if (cameraPermissionState.status.shouldShowRationale) {
@@ -36,5 +50,55 @@ fun RecordScreen() {
                 Text("Request permission")
             }
         }
+    }
+}
+
+@Composable
+fun CameraPreviewView() {
+    val coroutineScope = rememberCoroutineScope()
+    val lifecycleOwner = LocalLifecycleOwner.current
+    AndroidView(
+        factory = { context ->
+            val previewView = PreviewView(context).apply {
+                this.scaleType = scaleType
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+            }
+
+            // CameraX Preview UseCase
+            val previewUseCase = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(previewView.surfaceProvider)
+                }
+
+            coroutineScope.launch {
+                val cameraProvider = context.getCameraProvider()
+                try {
+                    // Must unbind the use-cases before rebinding them.
+                    cameraProvider.unbindAll()
+                    cameraProvider.bindToLifecycle(
+                        lifecycleOwner, CameraSelector.DEFAULT_BACK_CAMERA, previewUseCase
+                    )
+                } catch (ex: Exception) {
+                    Log.e("CameraPreview", "Use case binding failed", ex)
+                }
+            }
+
+            previewView
+        }
+    )
+}
+
+
+val Context.executor: Executor
+    get() = ContextCompat.getMainExecutor(this)
+suspend fun Context.getCameraProvider(): ProcessCameraProvider = suspendCoroutine { continuation ->
+    ProcessCameraProvider.getInstance(this).also { future ->
+        future.addListener({
+            continuation.resume(future.get())
+        }, executor)
     }
 }
